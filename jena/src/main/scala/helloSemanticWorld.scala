94cb9f74e143c29cc10e895cf7883fc88aaa106a
Markus Kohlhase <mail@markus-kohlhase.de>
2011-05-12 17:17:19 +0200

    refactored source and added logger

--- jena/src/main/scala/helloSemanticWorld.scala
+++ jena/src/main/scala/helloSemanticWorld.scala
@@ -4,61 +4,85 @@ import com.hp.hpl.jena.rdf.model._
 import com.hp.hpl.jena.util._
 import com.hp.hpl.jena.query._
 
+import java.io.InputStream
+
+import org.apache.commons.logging._
+
 object helloSemanticWorld{
+
+  private val log:Log = LogFactory.getLog( this.getClass )
 
   def main( args:Array[String] ){
 
-    val model = ModelFactory.createDefaultModel
-    println("repository initialized")
+    val prefix = "PREFIX so: <http://github.com/flosse/semanticExperiments/ontologies/simpleOntology#>"
+    val searchText = "sensor"
 
-    println("adding file content...")
-    val inputFileName = "../ontologies/simpleOntology.n3"
-    val in = FileManager.get().open( inputFileName )
+    val model = loadRDFData( "../ontologies/simpleOntology.n3", createEmptyModel )
+    query( searchText, prefix, model )
 
-    in match {
-      case null		=> println( "File: " + inputFileName + " not found" )
-      case _		=> read
-    }  
+  }
 
-    def read {
-      try{
-	model.read( in, null, "N3" )
-	println("content added")
-	query
-      }catch{
-	case e:Exception	=> println("error: " + e )
-	case _			=> println("something went wrong" )
-      }
-    }
+  def createEmptyModel = ModelFactory.createDefaultModel
 
-    def query {
+  def loadRDFData( fileName:String, model:Model ):Model = {
 
-      println("sample quering...")
+    log.debug("adding file content...")
 
-      var textSearchString = "sensor"
-      val prefix = "PREFIX so: <http://github.com/flosse/semanticExperiments/ontologies/simpleOntology#> "
-      val select = "SELECT DISTINCT ?s "
-      val where = "WHERE { ?s ?p ?o . FILTER ( regex( str(?s) , \"(?i)" + textSearchString + "\" ) ) } " 
-      val order = "ORDER BY ?s"
-      val queryString = prefix + select + where + order
+    FileManager.get().open( fileName ) match {
 
-      println("SPARQL: " + queryString )
+      case null		        => log.error( "File: " + fileName + " not found" ); model
+      case s:InputStream	=> loadFileIntoModel( s, model )
+      case _              => log.error( "something went wrong" ); model
 
-      val query = QueryFactory.create( queryString )
-      val qexec = QueryExecutionFactory.create( query, model )
-
-      try {
-        println("Result:")
-        val results = qexec.execSelect
-        while( results.hasNext ){
-          val soln = results.nextSolution
-          val x = soln.get("s")
-          println( x )
-        }
-      }catch{
-        case e:Exception	=> println("error: "+ e )
-        case _		=> println("something went wrong" )
-      } finally { qexec.close }
     }
   }
+
+  def loadFileIntoModel( in:InputStream, model:Model ):Model = {
+    try{
+      model.read( in, null, "N3" )
+      log.debug("content added")
+      model
+    }catch{
+      case e:Exception  => log.error( "error: " + e ); model
+      case _            => log.error( "something went wrong" ); model
+    } finally {
+      model
+    }
+  }
+
+  def query( searchText:String, prefix:String, model:Model ) {
+
+    val qexec = QueryExecutionFactory.create( createQuery( searchText, prefix ), model )
+
+    try {
+      log.info("Result:")
+      printResults( qexec.execSelect, "s" )
+    }catch{
+      case e:Exception	=> println("error: "+ e )
+      case _           	=> println("something went wrong" )
+    } finally { qexec.close }
+  }
+
+  def printResults( results:ResultSet, varName:String ){
+
+    while( results.hasNext ){
+      log.info( results.nextSolution.get( varName ) )
+    }
+  }
+
+  def createQuery( searchText:String, prefix:String ) = {
+
+    log.debug("create query")
+
+    val select = " SELECT DISTINCT ?s "
+    val regex = "regex( str(?s) , \"(?i)" + searchText + "\" )"
+    val where = "WHERE { ?s ?p ?o . FILTER ( " + regex + " ) } "
+    val order = "ORDER BY ?s"
+    val queryString = prefix + select + where + order
+
+    log.debug("Search for \"" + searchText + "\"" )
+
+    QueryFactory.create( queryString )
+  }
+
 }
